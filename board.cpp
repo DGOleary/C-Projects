fec1d98cea06875149b35fa9634e79f2775760f6
DGOleary <106465138+DGOleary@users.noreply.github.com>
2023-03-18 03:00:52 -0500

    Add files via upload

--- board.cpp
+++ board.cpp
@@ -42,6 +42,7 @@ for(int i=0;i<8;i++){
 }
 
 void makeMove(int x, int y, int xn, int yn){
+    
 if(player&&game_board[x][y]==("|X|")){
     if(x!=xn&&y!=yn){
        game_board[x][y]="|_|";
@@ -58,6 +59,13 @@ if(player&&game_board[x][y]==("|X|")){
     }
 }
 cout << "Illegal Move\n";
+}
+
+bool validMove(int x, int y, int xn, int yn){
+if(x<0||x>8||y<0||y>8||xn<0||xn>8||yn>0||yn<8){
+        cout << "Illegal Move\n";
+        return;
+    }
 }
 
 bool getWon(){
