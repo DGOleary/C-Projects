64e8aed22b3dc0501018706def9b31e0f38d7e2f
DGOleary <106465138+DGOleary@users.noreply.github.com>
2023-03-18 01:25:07 -0500

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -4,18 +4,37 @@
 
 using namespace std;
 
+bool moveCheck(std::string m){
+    if(m.length()>2){
+            cout << "Incorrect move, Word your moves as Letter, then Number of the spot, Example: A1\n";
+            return false;
+        }
+        char col = m[0];
+        if(col<65||col>72){
+            cout << "Incorrect move, Column must be between A and H\n";
+            return false;
+        }
+        int row = atoi(&m[1]);
+        if(row<1||row>8){
+            cout << "Incorrect move, Row must be between 1 and 8\n";
+            return false;
+        }
+        return false;
+}
+
 int main(){
+    
     board board;
     board.printBoard();
     board.makeMove(2,1,3,2);
     cout << "\n";
-    board.printBoard();
-    string t = "123456";
-    string s(1,t[0]), a(1,t[1]);
-    cout << stoi(s) <<"\n";
-    int x = stoi(s)+stoi(a);
-    cout << x <<"\n";
-    cout << "Word your moves as Letter, then Number of the spot, Example: A1";
+    // board.printBoard();
+    // string t = "123456";
+    // string s(1,t[0]), a(1,t[1]);
+    // cout << stoi(s) <<"\n";
+    // int x = stoi(s)+stoi(a);
+    // cout << x <<"\n";
+    cout << "Word your moves as Letter, then Number of the spot, Example: A1\n";
     while(!board.getWon()){
         string move;
         cout << "Current Player: ";
@@ -23,24 +42,17 @@ int main(){
             cout << "X's\n";
         }else{
             cout << "O's\n";
-        }
         cout << "Select piece to move: ";
         cin >> move;
-        if(move.length()>2){
-            cout << "Incorrect move, Word your moves as Letter, then Number of the spot, Example: A1";
-            continue;
-        }
-        char col = move[0];
-        if(col<65||col>72){
-            cout << "Incorrect move, Column must be between A and H";
-            continue;
-        }
-        int row = atoi(&move[1]);
-        if(row<1||row>8){
-            cout << "Incorrect move, Row must be between 1 and 8";
-            continue;
+        //cin.clear();
+        while(!moveCheck(move)){
+            cout << "Select piece to move: ";
+            cin >> move;
         }
 
    }
 }
+return 0;
+}
+
 
