cbf5fec6f17fa1fe96f93f3c6f8caca3b275ecee
DGOleary <106465138+DGOleary@users.noreply.github.com>
2023-03-18 16:22:16 -0500

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include "board.cpp"
+#include "Board.cpp"
 
 using namespace std;
 
@@ -8,12 +8,13 @@ bool moveCheck(string m, char *c, int *i);
 
 bool spotMove(string temp);
 
-int main(){
-    board board;
+    Board board;
     char col;
     int row;
     char n_col;
     int n_row;
+int main(){
+    
     cout << "Word your moves as Letter, then Number of the spot, Example: A1\n\n";
     while(!board.getWon()){
         string move;
@@ -36,30 +37,35 @@ int main(){
         cout << "Select spot to move to, r or l: ";
         cin >> move;
         cout << "\n";
-        while(!spotMove(move)){
+        
+  
+        bool run = false;
+while(!run){
+            while(!spotMove(move)){
             cout << "Select spot to move to, r or l: ";
             cin >> move;
             cout << move << " \n";
         }
-  
         if(board.getPlayer()){
         if(move=="r"){
-            board.makeMove(row-1, (int)col-65, row, ((int)col+1)-65);
+            run = board.makeMove(row-1, (int)col-65, row, ((int)col+1)-65);
         }else{
-            board.makeMove(row-1, (int)col-65, row, ((int)col-1)-65);
+            run = board.makeMove(row-1, (int)col-65, row, ((int)col-1)-65);
         }
         }else{
         if(move=="r"){
-            board.makeMove(row-1, (int)col-65, row-2, ((int)col+1)-65);
+            run = board.makeMove(row-1, (int)col-65, row-2, ((int)col+1)-65);
         }else{
-            board.makeMove(row-1, (int)col-65, row-2, ((int)col-1)-65);
+            run = board.makeMove(row-1, (int)col-65, row-2, ((int)col-1)-65);
         }
         }
-        
+        move="";
+        } 
 }
 return 0;
 }
 
+//todo: make sure piece has a valid possible move
 bool moveCheck(std::string m, char *c, int *i){
     if(m.length()!=2){
             cout << "Incorrect move, Word your moves as Letter, then Number of the spot, Example: A1\n";
@@ -74,6 +80,17 @@ bool moveCheck(std::string m, char *c, int *i){
         if(*i<1||*i>8){
             cout << "Incorrect move, Row must be between 1 and 8\n";
             return false;
+        }
+        if(board.getPlayer()){
+            if(board.getSpot(row-1,(int)col-65)=="|o|"){
+                cout << "Incorrect move, you are playing as \"X\"\n";
+                return false;
+            }
+        }else{
+                if(board.getSpot(row-1,(int)col-65)=="|x|"){
+                cout << "Incorrect move, you are playing as \"o\"\n";
+                return false;
+            }
         }
         return true;
 }
